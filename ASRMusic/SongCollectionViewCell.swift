ed663bfb2bbf3cdb8786bd5532f88456f48abf1c
Ayushman Singh Rajawat <ayushmanrajawatv@gmail.com>
2024-05-26 11:29:08 +0530

    collectionview app completed

---
+++ ASRMusic/SongCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  SongCollectionViewCell.swift
+//  ASRMusic
+//
+//  Created by Ayushman Singh Rajawat on 26/05/24.
+//
+
+import UIKit
+
+class SongCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var albumLabel: UILabel!
+    
+}
--- ASRMusic/ViewController.swift
+++ ASRMusic/ViewController.swift
@@ -7,56 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    @IBOutlet weak var table: UITableView!
     
-    var songs=[Song]()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configureSongs()
-        table.delegate=self
-        table.dataSource=self
-        // Do any additional setup after loading the view.
-    }
-    func configureSongs(){
-        songs.append(Song(name: "Background Music", artistName: "Rando", albumname: "ABC", imageName: "1cover", trackName: "song1"))
-        songs.append(Song(name: "Havana", artistName: "Camilla", albumname: "Havana album", imageName: "3cover", trackName: "song3"))
-        songs.append(Song(name: "Viva La Lida", artistName: "Coldplay", albumname: "XYZ", imageName: "2cover", trackName: "song2"))
-        songs.append(Song(name: "Background Music", artistName: "Rando", albumname: "ABC", imageName: "1cover", trackName: "song1"))
-        songs.append(Song(name: "Havana", artistName: "Camilla", albumname: "Havana album", imageName: "3cover", trackName: "song3"))
-        songs.append(Song(name: "Viva La Lida", artistName: "Coldplay", albumname: "XYZ", imageName: "2cover", trackName: "song2"))
-        songs.append(Song(name: "Background Music", artistName: "Rando", albumname: "ABC", imageName: "1cover", trackName: "song1"))
-        songs.append(Song(name: "Havana", artistName: "Camilla", albumname: "Havana album", imageName: "3cover", trackName: "song3"))
-        songs.append(Song(name: "Viva La Lida", artistName: "Coldplay", albumname: "XYZ", imageName: "2cover", trackName: "song2"))
-
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return songs.count
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let song = songs[indexPath.row]
-        cell.accessoryType = .disclosureIndicator
-        cell.textLabel?.text=song.name
-        cell.imageView?.image=UIImage(named: song.imageName)
-        cell.detailTextLabel?.text=song.albumname
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var songs = [Song]()
         
-        return cell
-    }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 90
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let position=indexPath.row
-        guard let vc = storyboard?.instantiateViewController(withIdentifier: "player") as? PlayerViewController else {
-            return
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            configureSongs()
+            collectionView.delegate = self
+            collectionView.dataSource = self
+            
+            // Register the custom cell
+            let nib = UINib(nibName: "cellFirst", bundle: nil)
+            collectionView.register(nib, forCellWithReuseIdentifier: "cellFirst")
         }
-        vc.songs = songs
-        vc.position = position
-        present(vc, animated: true)
+        
+        func configureSongs() {
+            songs.append(Song(name: "Background Music", artistName: "Rando", albumname: "ABC", imageName: "1cover", trackName: "song1"))
+            songs.append(Song(name: "Havana", artistName: "Camilla", albumname: "Havana album", imageName: "3cover", trackName: "song3"))
+            songs.append(Song(name: "Viva La Vida", artistName: "Coldplay", albumname: "XYZ", imageName: "2cover", trackName: "song2"))
+            songs.append(Song(name: "Background Music", artistName: "Rando", albumname: "ABC", imageName: "1cover", trackName: "song1"))
+            songs.append(Song(name: "Havana", artistName: "Camilla", albumname: "Havana album", imageName: "3cover", trackName: "song3"))
+            songs.append(Song(name: "Viva La Vida", artistName: "Coldplay", albumname: "XYZ", imageName: "2cover", trackName: "song2"))
+            songs.append(Song(name: "Background Music", artistName: "Rando", albumname: "ABC", imageName: "1cover", trackName: "song1"))
+            songs.append(Song(name: "Havana", artistName: "Camilla", albumname: "Havana album", imageName: "3cover", trackName: "song3"))
+            songs.append(Song(name: "Viva La Vida", artistName: "Coldplay", albumname: "XYZ", imageName: "2cover", trackName: "song2"))
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return songs.count
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellFirst", for: indexPath) as! SongCollectionViewCell
+            let song = songs[indexPath.row]
+            cell.nameLabel.text = song.name
+            cell.imageView.image = UIImage(named: song.imageName)
+            cell.albumLabel.text = song.albumname
+            return cell
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+            collectionView.deselectItem(at: indexPath, animated: true)
+            let position = indexPath.row
+            guard let vc = storyboard?.instantiateViewController(withIdentifier: "player") as? PlayerViewController else {
+                return
+            }
+            vc.songs = songs
+            vc.position = position
+            present(vc, animated: true)
+        }
+        
+        // UICollectionViewDelegateFlowLayout methods to adjust cell size
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            return CGSize(width: collectionView.frame.width, height: 90)
+        }
     }
-    
-}
-
